67b46a63c8fd97640f550bb750370746244b7476
Khip01 <aakhifathallah24@gmail.com>
2022-03-03 22:30:03 +0700

    Menambahkan File

---
+++ KONVERSI Nilai Des,Okt,Hex.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <conio.h>
+
+using namespace std;
+int main(){
+	int a;
+	cout<<"SELAMAT DATANG DI KONVERSI NILAI => DESIMAL"<<endl;
+	cout<<"                                 => OKTAL  "<<endl;
+	cout<<"                                 => HEXADESIMAL"<<endl;
+	cout<<"Masukkan Input   ="<<ends;cin>>a;cout<<endl;
+	cout<<"Nilai Desimal    ="<<ends<<dec<<a<<endl;	
+	cout<<"Nilai Oktal      ="<<ends<<oct<<a<<endl;
+	cout<<"Nilai Hexadesimal="<<ends<<hex<<a<<endl;
+	getch();
+}
